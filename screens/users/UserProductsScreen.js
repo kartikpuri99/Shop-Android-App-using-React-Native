3571af81522ef236a31137f62a8f30cc64c6c1d5
Kartik Puri <52126579+kartikpuri99@users.noreply.github.com>
2020-12-07 11:46:04 +0530

    commit

---
+++ screens/users/UserProductsScreen.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { FlatList, Platform, Button, Alert, View, Text } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../../components/UI/HeaderButton";
+import ProductItem from "../../components/shop/ProductItem";
+import Colors from "../../constants/Colors";
+import * as productsAction from "../../store/actions/products";
+const UserProductsScreen = (props) => {
+  const userProducts = useSelector((state) => state.products.userProducts);
+  const dispatch = useDispatch();
+  const editProductHandler = (id) => {
+    props.navigation.navigate("EditProduct", { productId: id });
+  };
+  const deleteHandler = (id) => {
+    Alert.alert("Are you sure?", "Do you want to delete this item?", [
+      { text: "No", style: "default" },
+      {
+        text: "Yes",
+        style: "destructive",
+        onPress: () => {
+          dispatch(productsAction.deleteProduct(id));
+        },
+      },
+    ]);
+  };
+  if (userProducts.length === 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>No Products found, maybe add some?</Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={userProducts}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onSelect={() => {
+            editProductHandler(itemData.item.id);
+          }}
+        >
+          <Button
+            color={Colors.primary}
+            title="Edit"
+            onPress={() => {
+              editProductHandler(itemData.item.id);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="Delete"
+            onPress={() => {
+              deleteHandler(itemData.item.id);
+            }}
+          />
+        </ProductItem>
+      )}
+    ></FlatList>
+  );
+};
+
+UserProductsScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Your Products",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS === "android" ? "md-create" : "ios-create"}
+          onPress={() => {
+            navData.navigation.navigate("EditProduct");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+export default UserProductsScreen;
